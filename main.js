ba55dbda6bbd63b1aea7f57d5959c5632a0f0350
aravinth8524 <aravinththiruppathi1999@gmail.com>
2023-08-03 02:42:28 -0700

    new

---
+++ main.js
@@ -0,0 +1,33 @@
+// 1, print n numbers in reverse order
+
+// let a=50;
+// for(i=a;i>=1;i--)
+// {
+	// console.log(i);
+// }
+
+// 2.print n odd numbers
+
+// let a=20;
+
+// for(i=1; i<=a; i++){
+  
+    
+    // if(i%2!=0){
+        // console.log(i);
+    // }
+    
+    
+// }
+
+// 4.print table of given number
+// let a=5;
+// for(i=1;i<=10;i++)
+// {
+	// let multi=0;
+	// multi=multi+i;
+	// console.log(i+"x"+a+"="+multi*a);
+// }
+
+// 3.
+
